e61147cdcdbed797cb5cc1f4d797a29900458e1e
RAFAEL-NOTEBOOK\Rafael <rafaelsmag@gmail.com>
2017-03-04 17:28:09 -0300

    Added the SelectionRedeucer and expanding the item list with the item description

--- tech_stack/src/components/ListItem.js
+++ tech_stack/src/components/ListItem.js
@@ -1,24 +1,77 @@
-import  React, { Component } from 'react';
-import { Text, StyleSheet } from 'react-native';
+import React, { Component } from 'react';
+import { 
+	Text, 
+	StyleSheet, 
+	TouchableWithoutFeedback, 
+	View, 
+	LayoutAnimation, 
+	Platform, 
+	UIManager 
+} from 'react-native';
+import { connect } from 'react-redux';
 import { CardSection } from './common';
+import * as actions from '../actions';
 
-class ListItem extends Component {
-	render() {
-		const { titleStyle } = styles;
-		return (
-			<CardSection>
-				<Text style={titleStyle}>{this.props.library.title}</Text>
-			</CardSection>
-			);
-	}
+if (Platform.OS === 'android') {
+  UIManager.setLayoutAnimationEnabledExperimental(true);
 }
 
+class ListItem extends Component {
+	componentWillUpdate() {
+		LayoutAnimation.spring();
+	}
+
+	renderDescription() {
+		const { library, expanded } = this.props;
+		const { descriptionStyle } = styles;
+		if (expanded) {
+			return (
+				<CardSection>
+					<Text style={descriptionStyle}>
+						{library.description}
+					</Text>
+				</CardSection>
+			);
+		}
+	}
+
+	render() {
+		const { titleStyle } = styles;
+		const { id, title } = this.props.library; 
+
+		return (
+			<TouchableWithoutFeedback
+				onPress={() => this.props.selectLibrary(id)}
+			>
+				<View>
+					<CardSection>
+						<Text style={titleStyle}>
+							{title}
+						</Text>
+					</CardSection>
+					{this.renderDescription()}
+				</View>
+			</TouchableWithoutFeedback>
+		);
+	}
+}
 
 const styles = StyleSheet.create({
 	titleStyle: {
 		fontSize: 18,
 		paddingLeft: 15
+	},
+	descriptionStyle: {
+		flex: 1,
+		paddingLeft: 8,
+		paddingRight: 8
 	}
 });
 
-export default ListItem;
+const mapStateToProps = (state, ownProps) => {
+	const expanded = state.selectedLibraryId === ownProps.library.id;
+
+	return { expanded };
+};
+
+export default connect(mapStateToProps, actions)(ListItem);
