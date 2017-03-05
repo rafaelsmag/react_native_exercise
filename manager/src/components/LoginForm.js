a96fcf27885abb27c1302773cc658b19dae2ea8d
RAFAEL-NOTEBOOK\Rafael <rafaelsmag@gmail.com>
2017-03-05 15:41:09 -0300

    Added the password and login on the AuthReducer

--- manager/src/components/LoginForm.js
+++ manager/src/components/LoginForm.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { emailChanged } from '../actions';
+import { emailChanged, passwordChanged, loginUser } from '../actions';
 import { Card, CardSection, Input, Button } from './common';
 
 class LoginForm extends Component {
 
 	onEmailChange(text) {
 		this.props.emailChanged(text);
+	}
+
+	onPasswordChange(text) {
+		this.props.passwordChanged(text);
+	}
+
+	onButtonPress() {
+		const { email, password } = this.props;
+
+		this.props.loginUser({ email, password });
 	}
 
 	render() {
@@ -17,6 +27,7 @@ class LoginForm extends Component {
 						label="Email"
 						placeholder="email@gmail.com"
 						onChangeText={this.onEmailChange.bind(this)}
+						value={this.props.email}
 					/>
 				</CardSection>
 				
@@ -25,11 +36,13 @@ class LoginForm extends Component {
 						secureTextEntry
 						label="Password"
 						placeholder="password"
+						onChangeText={this.onPasswordChange.bind(this)}
+						value={this.props.password}
 					/>
 				</CardSection>
 
 				<CardSection>
-					<Button>
+					<Button onPress={this.onButtonPress.bind(this)}>
 						Login
 					</Button>
 				</CardSection>
@@ -38,4 +51,15 @@ class LoginForm extends Component {
 	}
 }
 
-export default connect(null, emailChanged)(LoginForm);
+const mapStateToProps = state => {
+	const { email, password, user } = state.auth;
+	return {
+		email,
+		password,
+		user
+	};
+};
+
+export default connect(mapStateToProps, { 
+	emailChanged, passwordChanged, loginUser 
+})(LoginForm);
