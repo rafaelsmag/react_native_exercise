7c59f716a6b983280177a3acd6f9778d72d5bbde
RAFAEL-NOTEBOOK\Rafael <rafaelsmag@gmail.com>
2017-03-06 21:28:52 -0300

    Added the error mesage on login fail  and the spinner during the requisition

--- manager/src/components/LoginForm.js
+++ manager/src/components/LoginForm.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
+import { Text, StyleSheet } from 'react-native';
 import { connect } from 'react-redux';
 import { emailChanged, passwordChanged, loginUser } from '../actions';
-import { Card, CardSection, Input, Button } from './common';
+import { Card, CardSection, Input, Button, Spinner } from './common';
 
 class LoginForm extends Component {
 
@@ -17,6 +18,18 @@ class LoginForm extends Component {
 		const { email, password } = this.props;
 
 		this.props.loginUser({ email, password });
+	}
+
+	renderButton() {
+		if (this.props.loading) {
+			return <Spinner size='large' />;
+		} 
+
+		return (
+			<Button onPress={this.onButtonPress.bind(this)}>
+				Login
+			</Button>
+		);
 	}
 
 	render() {
@@ -41,22 +54,34 @@ class LoginForm extends Component {
 					/>
 				</CardSection>
 
+				<Text style={styles.errorTextStyle}>
+					{this.props.error}
+				</Text>
+
 				<CardSection>
-					<Button onPress={this.onButtonPress.bind(this)}>
-						Login
-					</Button>
+					{this.renderButton()}
 				</CardSection>
 			</Card>
 		);
 	}
 }
 
+const styles = StyleSheet.create({
+	errorTextStyle: {
+		fontSize: 20,
+		alignSelf: 'center',
+		color: 'red'
+	}
+});
+
 const mapStateToProps = state => {
-	const { email, password, user } = state.auth;
+	const { email, password, user, error, loading } = state.auth;
 	return {
 		email,
 		password,
-		user
+		user,
+		error,
+		loading
 	};
 };
 
