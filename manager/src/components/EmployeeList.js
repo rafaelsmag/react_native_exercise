5dfbbfdbe79a4911a27292439cfc3fe9bd6cafd9
RAFAEL-NOTEBOOK\Rafael <rafaelsmag@gmail.com>
2017-03-11 19:48:57 -0300

    Implemented the employeeList fetching data from firebase

--- manager/src/components/EmployeeList.js
+++ manager/src/components/EmployeeList.js
@@ -1,18 +1,47 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { ListView, Text } from 'react-native';
+import { connect } from 'react-redux';
+import { employeesFetch } from '../actions';
 
 class EmployeeList extends Component {
 
+	componentWillMount() {
+		this.props.employeesFetch();
+		this.createDataSource(this.props);
+	}
+
+	componentWillReceiveProps(nextProps) {
+		this.createDataSource(nextProps);
+	}
+
+	createDataSource({ employeesList }) {
+		const ds = new ListView.DataSource({
+			rowHasChanged: (r1, r2) => r1 !== r2
+		});
+		this.dataSource = ds.cloneWithRows(employeesList);
+	}
+
+	renderRow(employee) {
+		return <Text>{employee.name}</Text>;
+	}
+
 	render() {
 		return (
-			<View>
-				<Text>Employee List</Text>
-				<Text>Employee List</Text>
-				<Text>Employee List</Text>
-				<Text>Employee List</Text>
-			</View>
+			<ListView
+				enableEmptySections
+				dataSource={this.dataSource}
+				renderRow={this.renderRow}
+			/>
 		);
 	}
 }
 
-export default EmployeeList;
+const mapStateToProps = (state) => {
+	const employees = _.map(state.employees, (val, uid) => {
+		return { ...val, uid };
+	});
+	return { employeesList: employees };
+};
+
+export default connect(mapStateToProps, { employeesFetch })(EmployeeList);
