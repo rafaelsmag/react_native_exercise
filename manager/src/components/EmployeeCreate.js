ae1c3059b8f59a75a18d8a4792f4c0ad22d50e4c
RAFAEL-NOTEBOOK\Rafael <rafaelsmag@gmail.com>
2017-03-14 18:29:02 -0300

    Refactoring the employeeForm and creating the edit screen

--- manager/src/components/EmployeeCreate.js
+++ manager/src/components/EmployeeCreate.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { Picker, Text, StyleSheet } from 'react-native'; 
 import { Input, CardSection, Card, Button } from './common';
 import { employeeUpdate, employeeCreate } from '../actions';
+import EmployeeForm from './EmployeeForm';
 
 class EmployeeCreate extends Component {
 
@@ -12,43 +13,9 @@ class EmployeeCreate extends Component {
 	}
 
 	render() {
-		return (
+		return (	
 			<Card>
-				<CardSection>
-				<Input
-					label="Name"
-					placeholder="João"
-					value={this.props.name}
-					onChangeText={value => this.props.employeeUpdate({ prop: 'name', value })}
-				/>
-				</CardSection>
-
-				<CardSection>
-					<Input
-						label="Phone"
-						placeholder="(99) 99999-9999"
-						value={this.props.phone}
-						onChangeText={value => this.props.employeeUpdate({ prop: 'phone', value })}
-					/>
-				</CardSection>
-
-				<CardSection style={{ alignItems: 'center' }}>
-					<Text style={styles.pickerLabelStyle}>Shift</Text>
-					<Picker
-						style={{ flex: 3 }}
-						selectedValue={this.props.shift}
-						onValueChange={value => this.props.employeeUpdate({ prop: 'shift', value })}
-					>
-						<Picker.Item label="Monday" value="Monday" />
-						<Picker.Item label="Tuesday" value="Tuesday" />
-						<Picker.Item label="Wednesday" value="Wednesday" />
-						<Picker.Item label="Thursday" value="Thursday" />
-						<Picker.Item label="Friday" value="Friday" />
-						<Picker.Item label="Saturday" value="Saturday" />
-						<Picker.Item label="Sunday" value="Sunday" />
-					</Picker>
-				</CardSection>
-
+				<EmployeeForm {...this.props} />
 				<CardSection>
 					<Button onPress={this.onButtonPress.bind(this)}>
 						Create
@@ -58,14 +25,6 @@ class EmployeeCreate extends Component {
 		);
 	}
 }
-
-const styles = StyleSheet.create({
-	pickerLabelStyle: {
-		fontSize: 16,
-		paddingLeft: 20,
-		flex: 1
-	}
-});
 
 const mapStateToProps = (state) => {
 	const { phone, name, shift } = state.employeeForm;
