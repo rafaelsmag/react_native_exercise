d25caeb3e35fdf9ce5529017dca71514ba4918b2
RAFAEL-NOTEBOOK\Rafael <rafaelsmag@gmail.com>
2017-03-15 21:40:14 -0300

    Persisting updated employee data on firebase

--- manager/src/actions/EmployeeActions.js
+++ manager/src/actions/EmployeeActions.js
@@ -3,7 +3,8 @@ import { Actions } from 'react-native-router-flux';
 import { 
 	EMPLOYEE_UPDATE,
 	EMPLOYEE_CREATE,
-	EMPLOYEES_FETCH_SUCCESS
+	EMPLOYEES_FETCH_SUCCESS,
+	EMPLOYEE_SAVE_SUCCESS
 	} from './types';
 
 export const employeeUpdate = ({ prop, value }) => {
@@ -17,7 +18,7 @@ export const employeeCreate = ({ name, phone, shift }) => {
 	const { currentUser } = firebase.auth();
 	
 	return (dispatch) => {
-		firebase.database().ref(`/users/${currentUser.uid}/employess`)
+		firebase.database().ref(`/users/${currentUser.uid}/employees`)
 			.push({ name, phone, shift })
 			.then(() => {
 				dispatch({ type: EMPLOYEE_CREATE });
@@ -30,9 +31,21 @@ export const employeesFetch = () => {
 	const { currentUser } = firebase.auth();
 
 	return (dispatch) => {
-		firebase.database().ref(`/users/${currentUser.uid}/employess`)
+		firebase.database().ref(`/users/${currentUser.uid}/employees`)
 			.on('value', snapshot => {
 				dispatch({ type: EMPLOYEES_FETCH_SUCCESS, payload: snapshot.val() });
+			});
+	};
+};
+
+export const employeeSave = ({ name, phone, shift, uid }) => {
+	const { currentUser } = firebase.auth();
+	return (dispatch) => {
+		firebase.database().ref(`/users/${currentUser.uid}/employees/${uid}`)
+			.set({ name, phone, shift })
+			.then(() => { 
+				dispatch({ type: EMPLOYEE_SAVE_SUCCESS });
+				Actions.employeeList({ type: 'reset' });
 			});
 	};
 };
--- manager/src/components/EmployeeEdit.js
+++ manager/src/components/EmployeeEdit.js
@@ -2,7 +2,7 @@ import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import EmployeeForm from './EmployeeForm';
-import { employeeUpdate } from '../actions';
+import { employeeUpdate, employeeSave } from '../actions';
 import { Card, CardSection, Button } from './common';
 
 class EmployeeEdit extends Component {
@@ -12,16 +12,30 @@ class EmployeeEdit extends Component {
         });
     }
 
+    onButtonPress() {
+        const { name, shift, phone } = this.props;
+        this.props.employeeSave({ name, shift, phone, uid: this.props.employee.uid });
+    }
+    
     render() {
         return (
             <Card>
                 <EmployeeForm />
                 <CardSection>
-                    <Button> Save changes </Button>
+                    <Button onPress={this.onButtonPress.bind(this)}> Save changes </Button>
                 </CardSection>
             </Card>
         );
     }
 }
 
-export default connect(null, { employeeUpdate })(EmployeeEdit);
+const mapStateToProps = (state) => {
+    const { name, phone, shift } = state.employeeForm;
+    return {
+        name,
+        phone,
+        shift
+    };
+};
+
+export default connect(mapStateToProps, { employeeUpdate, employeeSave })(EmployeeEdit);
