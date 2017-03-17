bd2a6cff563edd7bc99ae2d8bc716ff9cfe7b531
Rafael S. Magalhaes <rafaelsmag@gmail.com>
2017-03-17 18:03:12 -0300

    Added the communications package, and the button to send sms to the employee

--- manager/src/components/EmployeeEdit.js
+++ manager/src/components/EmployeeEdit.js
@@ -1,6 +1,7 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Communications from 'react-native-communications';
 import EmployeeForm from './EmployeeForm';
 import { employeeUpdate, employeeSave } from '../actions';
 import { Card, CardSection, Button } from './common';
@@ -16,6 +17,12 @@ class EmployeeEdit extends Component {
         const { name, shift, phone } = this.props;
         this.props.employeeSave({ name, shift, phone, uid: this.props.employee.uid });
     }
+
+    onTextPress(){
+        const {phone, shift} = this.props;
+
+        Communications.text(phone, `Your upcoming shift is on ${shift}`);
+    }
     
     render() {
         return (
@@ -23,6 +30,11 @@ class EmployeeEdit extends Component {
                 <EmployeeForm />
                 <CardSection>
                     <Button onPress={this.onButtonPress.bind(this)}> Save changes </Button>
+                </CardSection>
+                <CardSection>
+                    <Button onPress={this.onTextPress.bind(this)}>
+                        Text Schedule
+                     </Button>
                 </CardSection>
             </Card>
         );
