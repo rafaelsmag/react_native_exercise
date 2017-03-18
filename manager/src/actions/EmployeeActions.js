bc5b3c79574b35d0d9575abcf0e9e4db848869a4
Rafael S. Magalhaes <rafaelsmag@gmail.com>
2017-03-18 18:52:16 -0300

    Added the option to delete an employee

--- manager/src/actions/EmployeeActions.js
+++ manager/src/actions/EmployeeActions.js
@@ -1,11 +1,11 @@
 import firebase from 'firebase';
 import { Actions } from 'react-native-router-flux';
-import { 
+import {
 	EMPLOYEE_UPDATE,
 	EMPLOYEE_CREATE,
 	EMPLOYEES_FETCH_SUCCESS,
 	EMPLOYEE_SAVE_SUCCESS
-	} from './types';
+} from './types';
 
 export const employeeUpdate = ({ prop, value }) => {
 	return {
@@ -16,7 +16,7 @@ export const employeeUpdate = ({ prop, value }) => {
 
 export const employeeCreate = ({ name, phone, shift }) => {
 	const { currentUser } = firebase.auth();
-	
+
 	return (dispatch) => {
 		firebase.database().ref(`/users/${currentUser.uid}/employees`)
 			.push({ name, phone, shift })
@@ -43,9 +43,21 @@ export const employeeSave = ({ name, phone, shift, uid }) => {
 	return (dispatch) => {
 		firebase.database().ref(`/users/${currentUser.uid}/employees/${uid}`)
 			.set({ name, phone, shift })
-			.then(() => { 
+			.then(() => {
 				dispatch({ type: EMPLOYEE_SAVE_SUCCESS });
 				Actions.employeeList({ type: 'reset' });
 			});
 	};
 };
+
+export const employeeDelete = ({ uid }) => {
+	const { currentUser } = firebase.auth();
+
+	return () => {
+		firebase.database().ref(`/users/${currentUser.uid}/employees/${uid}`)
+			.remove()
+			.then(() => {
+				Actions.employeeList({ type: 'reset' });
+			});
+	}
+}
--- manager/src/components/EmployeeEdit.js
+++ manager/src/components/EmployeeEdit.js
@@ -3,10 +3,19 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Communications from 'react-native-communications';
 import EmployeeForm from './EmployeeForm';
-import { employeeUpdate, employeeSave } from '../actions';
-import { Card, CardSection, Button } from './common';
+import { employeeUpdate, employeeSave, employeeDelete } from '../actions';
+import { Card, CardSection, Button, Confirm } from './common';
 
 class EmployeeEdit extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            showFireModal: false
+        };
+        this.toggleModal = this.toggleModal.bind(this);
+    }
+
     componentWillMount() {
         _.each(this.props.employee, (value, prop) => {
             this.props.employeeUpdate({ prop, value });
@@ -18,13 +27,28 @@ class EmployeeEdit extends Component {
         this.props.employeeSave({ name, shift, phone, uid: this.props.employee.uid });
     }
 
-    onTextPress(){
-        const {phone, shift} = this.props;
+    onTextPress() {
+        const { phone, shift } = this.props;
 
         Communications.text(phone, `Your upcoming shift is on ${shift}`);
     }
-    
+
+    onFirePress() {
+        this.toggleModal();
+    }
+
+    toggleModal() {
+        this.setState({ showFireModal: !this.state.showFireModal });
+    }
+
+    fireEmployee() {
+        const { employeeDelete, employee } = this.props;
+        employeeDelete(employee);
+        this.toggleModal();
+    }
+
     render() {
+        const { name } = this.props;
         return (
             <Card>
                 <EmployeeForm />
@@ -35,6 +59,14 @@ class EmployeeEdit extends Component {
                     <Button onPress={this.onTextPress.bind(this)}>
                         Text Schedule
                      </Button>
+                </CardSection>
+                <CardSection>
+                    <Button onPress={this.onFirePress.bind(this)}>
+                        Fire Employee
+                     </Button>
+                    <Confirm onAccept={this.fireEmployee.bind(this)} onDecline={this.toggleModal} visible={this.state.showFireModal}>
+                        Are you sure you want to fire {name}?
+                    </Confirm>
                 </CardSection>
             </Card>
         );
@@ -50,4 +82,4 @@ const mapStateToProps = (state) => {
     };
 };
 
-export default connect(mapStateToProps, { employeeUpdate, employeeSave })(EmployeeEdit);
+export default connect(mapStateToProps, { employeeUpdate, employeeSave, employeeDelete})(EmployeeEdit);
